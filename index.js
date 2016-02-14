0762005a5b393faf95121fe17b17d098cabac73c
Jose Luis <jlds161089@gmail.com>
2016-02-14 00:36:25 -0500

    Lectura de trama, codigo de deteccion de error

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
-var Util = require('./lib/util');
+/*var Util = require('./lib/util');
 
 var util = new Util() ;
 
-console.log(util.hexToBin('00')) ;
+console.log(util.hexToBin('00')) ;*/
+var checksum = 0 ;
+['00','85','63','08','00','00','04','B2','DE','1F','04','00','9E','00','20','01','00','00','00','00','69','6C','F7','AB','00','2F','1A','00','00','00','00','00','00','00','32','5C','00','04','02','06','9B','FD','E7','08','57','E2','25','02','F4','1C','00','00','36','00','00','00','DF','0B','09','32','10','0B','09','DC','07'].forEach(function(element){
+	checksum += parseInt(element, 16) ;
+});
+checksum = parseInt(checksum, 10).toString(16) ;
+console.log(checksum);
