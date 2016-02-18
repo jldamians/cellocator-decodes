11b22574a6739dae9e4b82bb3a949d6b4a96f5bb
Jose Luis <jlds161089@gmail.com>
2016-02-17 22:57:36 -0500

    Implementacion del ACK de confirmacion de trama

--- index.js
+++ index.js
@@ -2,6 +2,20 @@
 
 var command = require('./lib/commands') ;
 
-console.log('reset => ' + command.reset(517975, 0)) ;
-console.log('activateImmobilizer => ' + command.activateImmobilizer(517975, 118)) ;
-console.log('eraseTrackingLogFromMemory=> ' + command.eraseTrackingLogFromMemory(517975, 0)) ;
+
+	console.log('reset => ' + command.reset(517975, 0)) ;
+	// Result => 
+
+	console.log('activateImmobilizer => ' + command.activateImmobilizer(517975, 118)) ;
+	// Result => 
+
+	console.log('eraseTrackingLogFromMemory=> ' + command.eraseTrackingLogFromMemory(517975, 0)) ;
+	// Result => 	
+
+
+
+	var example01 = command.genericAcknowledgeMessage(575873, 20, 22);
+
+	console.log(example01);
+
+	// Result => 
