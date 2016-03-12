0be0e2cf0c59b1cad6d02d3603c2d981a8ad1770
jlds <jlds161089@gmail.com>
2016-03-12 16:20:14 -0500

    Aplicando patron modulo al modulo util.js

--- index.js
+++ index.js
@@ -1,26 +1,24 @@
 'use strict';
 
-/*
-	var command = require('./lib/commands') ;
 
-	console.log('reset => ' + command.reset(517975, 0)) ;
+	var commands = require('./lib/commands').Commands ;
 
-	console.log('activateImmobilizer => ' + command.activateImmobilizer(517975, 118)) ;
+	console.log('reset => ' + commands.reset(517975, 0)) ;
 
-	console.log('eraseTrackingLogFromMemory=> ' + command.eraseTrackingLogFromMemory(517975, 0)) ;
+	console.log('activateImmobilizer => ' + commands.activateImmobilizer(517975, 118)) ;
 
-	var example01 = command.genericAcknowledgeMessage(575873, 20, 22);
+	console.log('eraseTrackingLogFromMemory=> ' + commands.eraseTrackingLogFromMemory(517975, 0)) ;
+
+	var example01 = commands.genericAcknowledgeMessage(575873, 20, 22);
 
 	console.log(example01);
+
+/*
+	var Cellocator = require('./lib/cellocator.js').Cellocator;
+
+	var trama = '4d434750002168050000069ab61c041017002000e3040100bad42cb700925c03000000000000d9ed000402098227f6f7314860ff354901000000000032080119171d02e007c8';
+
+	var celo = new Cellocator(trama);
+
+	console.log(celo.database());
 */
-
-var Cellocator = require('./lib/cellocator.js').Cellocator;
-
-var trama = '4d434750002168050000069ab61c041017002000e3040100bad42cb700925c03000000000000d9ed000402098227f6f7314860ff354901000000000032080119171d02e007c8';
-
-var celo = new Cellocator(trama);
-console.log(celo.database());
-
-/*var objTrama = celo.database();
-
-console.log(objTrama);*/
