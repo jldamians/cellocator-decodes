1d7e4625a0964ebce011ac68bb02103577290669
jlds <jlds161089@gmail.com>
2016-03-12 18:53:08 -0500

    Agregando constantes y refactorizando fechas con moment

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 'use strict';
 
-
+/*
 	var commands = require('./lib/commands').Commands ;
 
 	console.log('reset => ' + commands.reset(517975, 0)) ;
@@ -12,13 +12,12 @@
 	var example01 = commands.genericAcknowledgeMessage(575873, 20, 22);
 
 	console.log(example01);
+*/
 
-/*
 	var Cellocator = require('./lib/cellocator.js').Cellocator;
 
-	var trama = '4d434750002168050000069ab61c041017002000e3040100bad42cb700925c03000000000000d9ed000402098227f6f7314860ff354901000000000032080119171d02e007c8';
+	var trama = '4d43475000bdda0b0000060ddf20041017002000e3c40000baeff3c6b6224502000000000000ea65000402090daec5f7cb302cff3357000038090000930a002a170c03e007c1';
 
 	var celo = new Cellocator(trama);
 
 	console.log(celo.database());
-*/
