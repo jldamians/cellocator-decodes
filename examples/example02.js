9d760b137f118e02e5499f93724ec11b475c000e
jlds <jlds161089@gmail.com>
2016-06-03 22:35:00 -0500

    [update] La serie y el numerador tienen que ser datos enteros

--- examples/example02.js
+++ examples/example02.js
@@ -3,8 +3,8 @@
 var GENERIC_COMMAND_TYPES = require('../index.js').constants.genericCommandTypes;
 var Generic = require('../').commands.generic;
 
-console.log(Generic('ABCDEF', 'FE', GENERIC_COMMAND_TYPES.reset));//reset
-console.log(Generic('AEIOU', 'AB', GENERIC_COMMAND_TYPES.status));//status
-console.log(Generic('ABCDEF', 'AB', GENERIC_COMMAND_TYPES.activateImmobilizer));//activateImmobilizer
+console.log(Generic(1208671, 10, GENERIC_COMMAND_TYPES.reset));//reset
+console.log(Generic(470683, 15, GENERIC_COMMAND_TYPES.status));//status
+console.log(Generic(354331, 5, GENERIC_COMMAND_TYPES.activateImmobilizer));//activateImmobilizer
 
 
