5692460ca233fee1c8e801a625f8073aede21ded
jlds <jlds161089@gmail.com>
2016-06-05 08:12:51 -0500

    [add] Example, imprime systemCode y messageType

---
+++ examples/example05.js
@@ -0,0 +1,8 @@
+'use strict';
+
+var Decode = require('../index.js').decode;
+var trama = '4d43475000d52c090000081cd81f041017002001c3040820bacacab3b67a050000000000000087bc00040209c25bb3f777f84dfffa1500000000000000000e03131704e0079f';
+var cello = Decode.init(trama, '127.0.0.1', '3000');
+
+console.log('systemCode =>', cello.systemCode());
+console.log('messageType =>', cello.messageType());
