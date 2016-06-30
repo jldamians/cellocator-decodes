708ab6ff4b4465fda8150b6a671b98bd56f4a48e
Jose Luis <jlds161089@gmail.com>
2016-06-29 19:19:02 -0500

    [update] Prototype para la decodificación del a trama

--- examples/example01.js
+++ examples/example01.js
@@ -1,7 +1,6 @@
-'use strict';
 
 var decode = require('../index.js').decode;
 var trama = '4d43475000d52c090000081cd81f041017002001c3040820bacacab3b67a050000000000000087bc00040209c25bb3f777f84dfffa1500000000000000000e03131704e0079f';
-var cello = decode(trama, '127.0.0.1', '3000');
+var cello = new decode(trama, '127.0.0.1', '3000');
 
 console.log(cello.database());
